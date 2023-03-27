0797c3b23b9bd7dc7fdf4e3415e7e46a9a8f67f0
klimeshke <klimenkojk@yandex.ru>
2023-03-27 21:47:55 +0300

    Ex11-16 done

---
+++ Seminar_2/Ex11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+
+int firstNum = num / 100;
+int thirdNum = num % 10;
+
+int result = firstNum * 10 + thirdNum;
+Console.WriteLine($"Число {result}");
+
+// Console.WriteLine($"Число {firstNum}{thirdNum}");
---
+++ Seminar_2/Ex16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли одно число квадратом другого
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
